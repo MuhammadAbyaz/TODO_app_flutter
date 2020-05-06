bb74bbddbe6dc568edb50b669517abf94f5c2963
unknown <abyazkhalid2005@gmail.com>
2020-05-06 19:43:35 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "TODO",
+      home: Todo(title: "TODO"),
+    );
+  }
+}
+
+class Todo extends StatelessWidget {
+  final String title;
+
+  Todo({Key key, this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurpleAccent,
+        centerTitle: true,
+        title: Text(title),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                "TODO",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                    color: Colors.white),
+              ),
+              decoration: BoxDecoration(color: Colors.deepPurpleAccent),
+            ),
+            ListTile(
+              title: Text("Settings"),
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Settings()));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Settings"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+    );
+  }
+}
